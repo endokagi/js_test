03e4ca4f680b74cef81374f0c524516ac28743a0
endokagi <38246228+endokagi@users.noreply.github.com>
2019-11-01 20:37:36 +0700

    convert baht to usd

---
+++ convert.js
@@ -0,0 +1,14 @@
+//Write a short program that convert currency from Thai baht to US dollar. 
+//Input can be only ﬂoat or integer  
+//Return value must be string with preﬁx ‘$’, ex. ‘$3.00’ 
+//Return value must show only two decimal points
+
+// ฿1 = 0.03 usd
+
+function convert(currency) {
+
+    return `$${(currency*0.03).toFixed(2)}`; 
+    
+}
+
+module.exports = convert;
---
+++ convert.test.js
@@ -0,0 +1,21 @@
+const convert = require('./convert.js');
+
+test("Change ฿1.00 to $0.03", () => {
+    expect(convert(1)).toEqual('$0.03');
+});
+
+test("Change ฿15.00 to $0.45", () => {
+    expect(convert(15.0)).toEqual('$0.45');
+});
+
+test("Change ฿30.00 to $0.90", () => {
+    expect(convert(30.00)).toEqual('$0.90');
+});
+
+test("Change ฿-99.0000000001 to $-2.97", () => {
+    expect(convert(-99.0000000001)).toEqual('$-2.97');
+});
+
+test("Change ฿0.00 to $0.00", () => {
+    expect(convert(0.00)).toEqual('$0.00');
+});
